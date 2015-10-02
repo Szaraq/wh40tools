febca0553c1f928a289a5c5a078db19f8e88ed45
Osik <Osik@Osik-PC.local>
2015-10-02 14:40:41 +0200

    master

---
+++ application/models/DbTable/Gracze.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application_Model_DbTable_Gracze extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'Gracze';
+
+
+}
+
---
+++ application/models/DbTable/Rundy.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application_Model_DbTable_Rundy extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'Rundy';
+
+
+}
+
